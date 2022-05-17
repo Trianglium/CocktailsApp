aed102b9f24e81c7326f538609a5d3e62e9af973
Jordan <jbrockerrudow@gmail.com>
2022-05-17 03:09:46 -0500

    Start New Cocktails Project, Install dependencies & Router setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
+import { CocktailDetailsComponent } from './cocktail-details/cocktail-details.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CocktailsListComponent,
+    CocktailDetailsComponent,
+    NotFoundPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
