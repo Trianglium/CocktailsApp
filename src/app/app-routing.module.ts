7fd4a8e1fea77b6e742acb6d93ce80a528bf8637
Jordan <jbrockerrudow@gmail.com>
2022-05-17 03:10:22 -0500

    Integrate Routing & Activated Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
+import { CocktailDetailsComponent } from './cocktail-details/cocktail-details.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'list',
+    component: CocktailsListComponent
+  },
+  {
+    path: 'cocktail-details',
+    component: CocktailDetailsComponent
+  },
+  {
+    path: 'cocktail-details/:id',
+    component: CocktailDetailsComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
