d45d300c7ce71eb4f9df46df30ee9b0bd5b5f3c8
Jordan <jbrockerrudow@gmail.com>
2022-05-19 08:26:43 -0500

    Integrate Auth Service into main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  loggedIn: boolean;
   title = 'cocktails-app';
+
+
+
+  constructor(
+    private auth: AuthService
+  ) { }
+
+  logIn() {
+    this.auth.logIn();
+    this.loggedIn = true;
+  }
+  logOut() {
+    this.auth.logOut();
+    this.loggedIn = false;
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
