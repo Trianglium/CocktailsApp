6b1d09339a2159ae93a65e367d7ea3db8cecc1f4
Jordan <jbrockerrudow@gmail.com>
2022-05-19 11:25:59 -0500

    Integrate Auth Service Authentication

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(): true | UrlTree {
+    return this.authenticateOrRedirect();
+  }
+
+  private authenticateOrRedirect() {
+
+    if (this.authService.isAuthenticated()) {
+      return true;
+    } else {
+      return this.router.parseUrl('/unauthorized');
+    }
   }
   
 }
