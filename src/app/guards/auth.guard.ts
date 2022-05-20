12ae92915f2fda2f08e3a01ec9a784391bd8d632
Jordan <jbrockerrudow@gmail.com>
2022-05-20 05:01:37 -0500

    Update necessary imports

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
