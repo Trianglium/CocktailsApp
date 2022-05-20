a031e1b404322f8279f30442897d56df3e1abf12
Jordan <jbrockerrudow@gmail.com>
2022-05-20 08:03:13 -0500

    Material Icons in Side Nav & Fix Users Template Breadcrumbs

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,18 +5,27 @@ import { UsersComponent } from './users/users.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminComponent } from './admin.component';
+import {MatIconModule} from '@angular/material/icon';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { InfoComponent } from './info/info.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
     UsersComponent,
-    DashboardComponent
+    DashboardComponent,
+    AdminHomeComponent,
+    SettingsComponent,
+    InfoComponent
   ],
   imports: [
     RouterModule,
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatIconModule
+    
   ],
   providers: []
 })
